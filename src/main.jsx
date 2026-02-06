c4889be0178696eac937ad51be66339822eb9536
mhdkii <exchangepro19@gmail.com>
2026-02-06 16:50:09 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// --- PENTING: KODE UNTUK NYALAKAN PWA ---
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("Versi baru tersedia! Muat ulang?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("Aplikasi siap bekerja offline!");
+  },
+})
+// ------------------------------------------
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
