30e5391506a2255119261c86bd104a18bceed684
mhdkii <exchangepro19@gmail.com>
2026-02-06 14:43:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Pastikan index.css ada walaupun kosong
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import './index.css' // <--- Pastikan baris ini ada!
